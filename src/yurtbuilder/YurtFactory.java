876ab9a62cc9ecc2532b7087f2bd90500ce93b31
makhan8sabina <makhan.sabina@gmail.com>
2026-09-21 10:55:10 +0500

    Update YurtFactory.java

--- src/yurtbuilder/YurtFactory.java
+++ src/yurtbuilder/YurtFactory.java
@@ -1,10 +1,3 @@
-package yurtbuilder;
-
-/**
- * Abstract Factory: one creation method per product type in the
- * family (Frame, Stove). Guarantees that whatever family a client
- * picks, the resulting Frame and Stove are compatible with each other.
- */
 public interface YurtFactory {
     Frame createFrame();
     Stove createStove();
