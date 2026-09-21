98aebce4c754047db89d1c87bedd8ba78ed95308
makhan8sabina <makhan.sabina@gmail.com>
2026-09-21 10:49:20 +0500

    Update Client.java

--- src/yurtbuilder/Client.java
+++ src/yurtbuilder/Client.java
@@ -1,10 +1,3 @@
-package yurtbuilder;
-
-/**
- * Client. Works only through abstract types - Frame, Stove, YurtFactory -
- * and never instantiates a concrete class (SteppeFrame, MountainStove, ...)
- * directly.
- */
 public class Client {
 
     public static void main(String[] args) {
